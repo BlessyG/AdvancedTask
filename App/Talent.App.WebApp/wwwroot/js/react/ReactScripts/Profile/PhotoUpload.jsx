fe0d5a69f383ef9954804b1e924cb9bdd2a67709
Blessy Gnanamanickam <blessy.g.nz@gmail.com>
2020-04-14 12:46:58 +1200

    removed debugger

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/PhotoUpload.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/PhotoUpload.jsx
@@ -84,7 +84,6 @@ export default class PhotoUpload extends Component {
     }
 
     render() {
-        debugger
         let showProfileImg = [];
         let profileUrl = this.state.imageSrc ? this.state.imageSrc : this.props.imageId;
         //let test = `url(${URL.createObjectURL(this.props.imageId)}`;
